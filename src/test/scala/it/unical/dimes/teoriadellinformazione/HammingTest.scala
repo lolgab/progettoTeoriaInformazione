343b660d25c74b924adebacda7acc2d6d3cdf498
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2018-02-15 15:45:20 +0100

    Test prima testava solo cambiando il bit in posizione 2. Ora è un indice random tra quelli non di parità.

--- src/test/scala/it/unical/dimes/teoriadellinformazione/HammingTest.scala
+++ src/test/scala/it/unical/dimes/teoriadellinformazione/HammingTest.scala
@@ -4,13 +4,16 @@ import utest._
 
 object HammingTest extends TestSuite {
   implicit def toBoolean(i: Int) = i != 0
+  implicit def toInt(b: Boolean) = if(b) 1 else 0
   import Hamming._
+  def swapBit(d: Data, index: Int): Unit = d(index) = !d(index)
+
   val tests = Tests {
-    "r dovrebbe essere 3 per n = 4: Hamming(7,4)" - {
-      val n = 4
-      val r1 = r(4)
-      assert(r1 == 3)
-    }
+    // "r dovrebbe essere 3 per n = 4: Hamming(7,4)" - {
+    //   val n = 4
+    //   val r1 = r(4)
+    //   assert(r1 == 3)
+    // }
     'hamming74 - {
       val data: Data = Array(1,1,0,1)
       val calculated = encode(data).toList
@@ -23,6 +26,42 @@ object HammingTest extends TestSuite {
       val inserted: List[Boolean] = List(0,1,1,1,0,0,1,0,1,0,1,0)
       val calculated = encode(data).toList
       assert(calculated == calculated)
+    }
+
+    "encode e decode dovrebbero coincidere" - {
+      val data: Data = Array(1,1,1,0,1,0,1,0,1,0,0,0,0,1,0,1,1,0)
+      val encoded = encode(data)
+      val decoded = decode(encoded).toList.map(_.toInt)
+      val dataList = data.toList.map(_.toInt)
+      assert(dataList == decoded)
+    }
+
+    "encode e decode array generati random" - {
+      import scala.util.Random
+      for(j <- 1 until 100) {
+        val data: Data = new Array(Random.nextInt(1000))
+        for(i <- 0 until data.length) data(i) = Random.nextBoolean
+        val encoded = encode(data)
+        val decoded = decode(encoded).toList.map(_.toInt)
+        val dataList = data.toList.map(_.toInt)
+        assert(dataList == decoded)
+      }
+    }
+
+    "encode e decode con swap di un bit dei dati. Array generati random" - {
+      import scala.util.Random
+      for(j <- 1 until 1000) {
+        val length = Random.nextInt(1000) + 1
+        val data: Data = new Array(length)
+        for(i <- 0 until data.length) data(i) = Random.nextBoolean
+        val encoded = encode(data)
+        val (pI, dI) = parityDataIndexes(encoded.length)
+        val swappedIndex = dI(Random.nextInt(dI.length))
+        swapBit(encoded, swappedIndex)  // swap a random bit
+        val decoded = decode(encoded).toList.map(_.toInt)
+        val dataList = data.toList.map(_.toInt)
+        assert(dataList == decoded)
+      }
     }
   }
 }
